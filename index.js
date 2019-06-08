accd1fdba360c0de7056b0ad8fe29705ffc33fdc
Fernanda Romero <fernandaromero85@gmail.com>
2019-06-08 01:06:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,11 +26,9 @@ function makeInt(n) {
   return parseInt(n)
 }
 
-/* describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
-
+function makeInt(n) {
+  
+}
   it('assumes base 10', function() {
     expect(makeInt('0x2328')).toEqual(0)
   })
@@ -38,4 +36,4 @@ function makeInt(n) {
   it('returns NaN as appropriate', function() {
     expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
   })
-}) */
+}) 
